a90654be3140e8ca4f8a1af22d73f3e50d10d87e
Alex Burner <alex.burner@gmail.com>
2016-11-27 12:33:09 -0800

    only using node env production flag in build, not dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
 
+/**
+ * Notes
+ * - NODE_ENV set to 'production' in package.json, for React optimization
+ * - the build folder is named "docs" to work with GitHub Pages
+ */
+
 module.exports = {
   resolve: {
     root: [
@@ -20,27 +26,27 @@ module.exports = {
     filename: 'docs/[name]/bundle.js'
   },
   plugins: [
+  new webpack.DefinePlugin({
+      'process.env': {'NODE_ENV': JSON.stringify(process.env.NODE_ENV)}
+    }),
     new webpack.optimize.CommonsChunkPlugin(
       // this is where the common built chunk lives
       'docs/shared/bundle.js'
     ),
-    new webpack.DefinePlugin({
-      // this tells react to use its production mode
-      'process.env': {'NODE_ENV': JSON.stringify('production')}
-    }),
     new webpack.optimize.DedupePlugin(),
-    new webpack.SourceMapDevToolPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       // this tells uglify to stfu about react
-      compress: { warnings: false },
+      compress: {warnings: false},
     }),
+    new webpack.SourceMapDevToolPlugin(),
   ],
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        include: /src/,
+        exclude: /node_modeules/,
+        test: /.jsx?$/,
         query: {
           presets: ['es2015', 'react']
         }
