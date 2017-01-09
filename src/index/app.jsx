af9da76fd0549e15d0189842181fc6507b1a4608
Alex Burner <alex.burner@gmail.com>
2017-01-09 00:26:41 -0800

    starting index

--- src/index/app.jsx
+++ src/index/app.jsx
@@ -2,18 +2,46 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import fireapp from 'shared/fireapp.jsx';
+
+import Loading from 'shared/loading.jsx';
+import TopNav from 'shared/top-nav.jsx';
+
 class App extends React.Component {
-  render() {
-    return (
-        <div>
-            <h1>Hello index app</h1>
-            <ul>
-                {['all', 'edit', 'random'].map(name => (
-                    <li><a href={name}>{name}</a></li>
-                ))}
-            </ul>
-        </div>
+  constructor() {
+    super();
+    this.unsubscribes = [];
+    this.state = {
+      userLoaded: false,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribes.push(
+      fireapp.auth().onAuthStateChanged((user) => {
+        this.setState({
+          userLoaded: true,
+          user: user,
+        });
+      })
     );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribes.forEach((fn) => fn());
+  }
+
+  render() {
+    return !this.state.userLoaded ?
+      <Loading /> :
+      <div className="container">
+        <TopNav user={this.state.user}/>
+        <h1 className="pull-left">
+            quote.garden
+        </h1>
+      </div>
+    ;
   }
 }
 
