8b7218359fb151d2a755eb970ab7be475a8c0ffe
Alex Burner <alex.burner@gmail.com>
2017-08-26 12:31:25 -0700

    Install firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,11 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import App from 'src/components/App'
+import fireapp from 'src/singletons/fireapp'
 import reducer from 'src/singletons/reducer'
 import { getInitState } from 'src/singletons/state'
+
+console.log(fireapp)
 
 const store = createStore(reducer, getInitState())
 
---
+++ src/singletons/fireapp.ts
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const fireapp = firebase.initializeApp({
+  apiKey: 'AIzaSyDR2zyCGLTU9cx6SZmsTvzUq31DLfoAd3U',
+  authDomain: 'quotes-92672.firebaseapp.com',
+  databaseURL: 'https://quotes-92672.firebaseio.com',
+  storageBucket: 'quotes-92672.appspot.com',
+  messagingSenderId: '936175857202',
+})
+
+export default fireapp
+export { fireapp, firebase }
